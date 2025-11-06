f105d005369bc07054a9eac9daf2b36d6f2cd4b6
manel1000 <manelnad018@gmail.com>
2025-11-06 19:03:27 +0100

    git commit -m "travaille terminé "

---
+++ .github/exo1/script.js
@@ -0,0 +1,39 @@
+
+    "use strict";
+    //déclaration de l'objet personne , comme dans le  slid 39
+    const person = {
+        nom : 'JOHN',
+        prenom : 'Doe',
+        age : 30,
+        note : 15 ,
+        Pi : 3.14 ,
+
+    }
+
+    //declaration des fonction , comme dans le slid 45
+    function f1() {
+        console.log(person.nom);
+    }
+
+    function f2() {
+        console.log(person.prenom);
+    }
+
+    function f3() {
+        console.log(person.nom + '-' + person.prenom);
+    }
+
+    function f4() {
+        console.log(person.age);
+    }
+
+    function f5() {
+        console.log(person.note);
+    }
+
+    function f6() {
+        console.log(person.Pi);
+    }
+
+
+
---
+++ .github/exo3/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+//déclaration de l'objet etudiants , comme dans le slide 39
+const tabEtudiants=[{
+        matricule : '1000',
+        nom : 'JOHN',
+        prenom : 'DOE',
+        note : 14,
+},
+
+{                                //déclaration du tableau  etudiants ,comme dans le slide 38
+        matricule : '2000',
+        nom : 'BOB',
+        prenom : 'CARLTON',
+        note : 7.1 ,
+},
+
+{
+        matricule : '3000',
+        nom : 'RAYANE',
+        prenom : 'SMITH',
+        note : 13.3,
+}]
+
+//déclaration de B, comme dans le slide 45
+function B(note){
+        if (note >=10){
+            console.log("ADMIS");
+        }
+   
+        else {
+            console.log("AJOURNE");
+        }
+}
+//déclaration de A avec un callback , comme dans le slide 47
+function A(callback){
+    for (let i=0; i<tabEtudiants.length; i++){
+       console.log(tabEtudiants[i].matricule);
+       callback(tabEtudiants[i].note );
+    }
+   
+}
